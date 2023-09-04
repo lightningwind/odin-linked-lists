0fc0271f4d8b77307316996b150530ede4af55d0
David Islam <david.islam24@gmail.com>
2023-09-03 19:00:07 -0700

    Add required methods to be implemented

---
+++ linked-list.js
@@ -0,0 +1,63 @@
+class LinkedList {
+  
+  /* Adds a new node containing value <value> to the end of this list. */
+  append(value) {
+
+  }
+
+  /* Adds a new node containing value <value> to the beginning of this list. */
+  prepend(value) {
+
+  }
+
+  /* Returns the total number of nodes in this list. */
+  size() {
+
+  }
+
+  /* Returns the first node in this list. */
+  head() {
+
+  }
+
+  /* Returns the last node in this list. */
+  tail() {
+
+  }
+
+  /* Returns the node at the given index. */
+  at(index) {
+
+  }
+
+  /* Removes the last element from this list. */
+  pop() {
+
+  }
+
+  /* Returns true iff the value passed in is in the list. */
+  contains(value) {
+
+  }
+
+  /* Returns the index of the node containing the given value, null otherwise. */
+  find(value) {
+
+  }
+
+  /* Returns a string representation of the entire linked list in the format: 
+  ( value ) -> ( value ) -> ( value ) -> null
+  */
+  toString() {
+
+  }
+
+
+}
+
+class Node {
+  value = null;
+  nextNode = null;
+
+  // TODO
+}
